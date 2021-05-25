311a4df8905c09208df633b521803f19afc09295
David-Antunes <david.m.m.antunes@gmail.com>
2021-05-25 15:06:39 +0100

    Add water to edges

--- gen_tests.py
+++ gen_tests.py
@@ -1,9 +1,7 @@
 from random import random,randint
-import time
-import sys
 
-# f = open("teste","w")
-tests = 1000
+
+tests = 1
 
 chars = ['G','O','W']
 def getRandom():
@@ -14,27 +12,26 @@ def getRandom():
         return 2
     else:
         return 0
-
 print(tests)
-# f.write(str(tests))
-# f.flush()
 for i in range(tests):
-    # time.sleep(0.1)
-    rows = randint(3,60)
-    cols = randint(3,60)
+    rows = randint(3,15)
+    cols = randint(3,15)
     wheels = randint(0,10)
     num_endings = 0
     num_wheels = 1
 
     print(str(rows) + " " + str(cols) + " " + str(wheels))
-    # f.write(str(rows) + " " + str(cols) + " " + str(wheels))
+
     world_map = []
     for row in range(rows):
         world_map.append([])
 
     for row in range(rows):
         for col in range(cols):
-            letter = chars[getRandom()]
+            if(row != 0 and col != 0 and row != rows - 1 and col != cols -1):    
+                letter = chars[getRandom()]
+            else:
+                letter = 'W'
             # if letter == 'T' and num_wheels <= 9:
             #     letter = str(num_wheels)
             #     num_wheels += 1
@@ -61,16 +58,15 @@ for i in range(tests):
         line = ''
         for letter in world_map[row]:
             line += letter
-        print(line, flush=True)
-        # f.write(line)
+        print(line)
+
     num_wheels = 0
     while(num_wheels < wheels):
         posX = randint(0, rows-1)
         posY = randint(0, cols-1)
         weight = randint(-100000, 100000)
         num_wheels += 1
-        print( str(posX) + " " + str(posY) + " " + str(weight), flush=True)
-        # f.write(str(posX) + " " + str(posY) + " " + str(weight))
+        print( str(posX) + " " + str(posY) + " " + str(weight))
 
     num_persons = 0
     while(num_persons < 2):
@@ -79,10 +75,7 @@ for i in range(tests):
         posX2 = randint(0, rows-1)
         posY2 = randint(0, cols-1)
         if(world_map[posX1][posY1] == 'G' and world_map[posX2][posY2] == 'G' and posX1 != posX2 and posY1 != posY2):
-            print(str(posX1) + " " + str(posY1) + " " + str(posX2) + " " + str(posY2), flush=True)  
-            # f.write(str(posX1) + " " + str(posY1) + " " + str(posX2) + " " + str(posY2))
+            print(str(posX1) + " " + str(posY1) + " " + str(posX2) + " " + str(posY2))  
             num_persons += 2
-    # f.flush()
-    sys.stdout.flush()
+    
 
-# f.close()
